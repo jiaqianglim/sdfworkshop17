f19d8410a5d887d93920b3611081ff795e88fdbe
jiaqianglim <jqlimjiaqiang@gmail.com>
2022-07-28 11:36:30 +0800

    first commit

---
+++ src/main/java/com/sdfworkshop17/Sdfworkshop17Application.java
@@ -0,0 +1,13 @@
+package com.sdfworkshop17;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdfworkshop17Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sdfworkshop17Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/sdfworkshop17/Sdfworkshop17ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sdfworkshop17;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdfworkshop17ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
